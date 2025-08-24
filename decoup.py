da684fc432fa24bf65e2891c2c31bc437868b2b7
TheoGaG <tgagneux.44700@gmail.com>
2025-08-24 16:59:59 +0200

    Modif2

--- decoup.py
+++ decoup.py
@@ -1,26 +1,26 @@
 import os
-from moviepy import VideoClip, VideoFileClip
+from moviepy import VideoFileClip
 
 # ---- Fichier vidéo à découper ---- #
 input_folder = "VideoFinis"
-output_folder = os.path.join(input_folder, "segment")
+output_folder = os.path.join(input_folder, "segments")
 
-# Crée le dossier s'y il n'existe pas
+# Crée le dossier s'il n'existe pas
 os.makedirs(output_folder, exist_ok=True)
 
 # Charge la vidéo
-video = VideoFileClip("VideoFinale1080p.mp4")
-duration = int(video.duration) # durée en seconde
+video_path = os.path.join(input_folder, "VideoFinale1080p.mp4")
+video = VideoFileClip(video_path)
+duration = int(video.duration)  # durée en secondes
 
 # Boucle pour découper segments de 60s
 segment_length = 60
 for i, start in enumerate(range(0, duration, segment_length)):
-  end = min(start + segment_length, duration)
-  segment = video.subclipped(start, end)
-  segment_filename = os.path.join(output_folder, f"segment_{i+1:03d}.mp4")
-  segment.write_videofile(segment_filename, codec="libx264", audio="aac")
-  segment.close()
+    end = min(start + segment_length, duration)
+    segment = video.subclipped(start, end)
+    segment_filename = os.path.join(output_folder, f"segment_{i+1:03d}.mp4")
+    segment.write_videofile(segment_filename, codec="libx264", audio_codec="aac")
+    segment.close()
 
 video.close()
-
-print(f"✅ Découpage terminé ! Segments dans : {output_folder}.")
+print(f"✅ Découpage terminé ! Segments dans : {output_folder}")
